24ce0c0d23a77ed466f8bd6840553ac113f8463f
jaysonb485 <jayson.barber@gmail.com>
2026-04-04 20:31:38 +1100

    Refactor AddWebAwesome method to remove parameters
    Removed parameters from AddWebAwesome method.

--- Config.cs
+++ Config.cs
@@ -4,11 +4,10 @@ namespace WebAwesomeBlazor
 {
     public static class Config
     {
-        public static IServiceCollection AddWebAwesome(this IServiceCollection services, string KitCode, string WebAwesomeVersionCode = "3.5.0")
+        public static IServiceCollection AddWebAwesome(this IServiceCollection services)
         {
             services.AddScoped<Extended.IConfirmDialog, Extended.ConfirmDialogService>();
             services.AddScoped<ToastService>();
-            services.AddScoped<IWebAwesomeOptions>(x => new WebAwesomeOptions(KitCode, WebAwesomeVersionCode));
             return services;
         }
 
